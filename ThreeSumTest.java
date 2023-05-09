880998d2fdaaec1f5a0724dc5c2f727402e36913
evanwitt14 <71992361+evanwitt14@users.noreply.github.com>
2023-05-09 16:52:03 -0400

    3Sum

---
+++ ThreeSumTest.java
@@ -0,0 +1,34 @@
+import java.util.*;
+
+public class ThreeSumTest {
+    public static void main(String[] args) {
+        int[] nums = { 1, 2, 3, 0, -1, -2, -3 };
+        List<List<Integer>> result = threeSum(nums);
+        System.out.println(result);
+    }
+
+    public static List<List<Integer>> threeSum(int[] nums) {
+        Arrays.sort(nums);
+        List<List<Integer>> result = new ArrayList<>();
+        for (int i = 0; i < nums.length - 2; i++) {
+            if (i == 0 || (i > 0 && nums[i] != nums[i - 1])) {
+                int lo = i + 1, hi = nums.length - 1, sum = 0 - nums[i];
+                while (lo < hi) {
+                    if (nums[lo] + nums[hi] == sum) {
+                        result.add(Arrays.asList(nums[i], nums[lo], nums[hi]));
+                        while (lo < hi && nums[lo] == nums[lo + 1])
+                            lo++;
+                        while (lo < hi && nums[hi] == nums[hi - 1])
+                            hi--;
+                        lo++;
+                        hi--;
+                    } else if (nums[lo] + nums[hi] < sum)
+                        lo++;
+                    else
+                        hi--;
+                }
+            }
+        }
+        return result;
+    }
+}
